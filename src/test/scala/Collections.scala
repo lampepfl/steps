6e80991957988bb680d3b554c72601c8b790d8cd
Natsu Kagami <nki@nkagami.me>
2024-04-10 16:03:28 +0200

    Add collection test

---
+++ src/test/scala/Collections.scala
@@ -0,0 +1,49 @@
+import result.Result
+import result.Result.*
+import result.Result.eval.*
+
+/** Tests working with collection. */
+class CollectionTest extends munit.FunSuite {
+  given [T]: Conversion[T, T] = identity
+
+  test("tryMap on iterable") {
+    extension [T](s: Iterable[T])
+      def tryMap[U, E](f: T => Result[U, E]) =
+        Result:
+          s.map(f(_).?)
+
+    def getEven(v: Int) =
+      if v % 2 == 0 then Ok(v) else Err("not even number")
+
+    val mustEvens =
+      (1 to 5)
+        .tryMap(getEven)
+        .map(_.toSeq)
+
+    assertEquals(mustEvens, Err("not even number"))
+
+    val evens =
+      (1 to 5)
+        .map(getEven)
+        .flatMap(_.ok)
+
+    assertEquals(evens, Seq(2, 4).toIndexedSeq)
+  }
+
+  test("tryMap on tree") {
+    enum Tree[T]:
+      case Leaf(item: T)
+      case Node(item: T, children: Tree[T]*)
+
+    extension [T](t: Tree[T])
+      def map[U](f: T => U): Tree[U] =
+        t match
+          case Tree.Leaf(item) => Tree.Leaf(f(item))
+          case Tree.Node(item, children*) =>
+            Tree.Node(f(item), children.map(_.map(f))*)
+
+      def tryMap[U, E](f: T => Result[U, E]) =
+        Result:
+          t.map(f(_).?)
+  }
+}
