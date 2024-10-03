f02a0fb875f067b77556ac5074f73bc30a01c5f7
Hamza Remmal <hamza@remmal.dev>
2024-10-03 23:20:53 +0200

    First draft of the @memoize

---
+++ src/main/scala/steps/annotation/memoize.scala
@@ -0,0 +1,50 @@
+package steps.annotation
+
+import scala.annotation.{experimental, MacroAnnotation}
+import scala.quoted.*
+import scala.collection.mutable.Map
+
+@experimental
+class memoize extends MacroAnnotation:
+  def transform(using Quotes)(definition: quotes.reflect.Definition): List[quotes.reflect.Definition] =
+    import quotes.reflect.*
+    definition match
+      case DefDef(name, params@List(TermParamClause(List(param))), tpt, Some(rhs)) =>
+        (param.tpt.tpe.asType, tpt.tpe.asType) match
+          case ('[k], '[v]) =>
+            val cache = buildCache[k,v](Symbol.freshName(s"${name}Cache"))
+            val newRHS = 
+              adaptDefToCache(using definition.symbol.asQuotes)
+                             (Ref(cache.symbol).asExprOf[Map[k, v]], 
+                              Ref(param.symbol).asExprOf[k], 
+                              rhs.asExprOf[v]
+                              )
+            List(cache, DefDef.copy(definition)(name, params, tpt, Some(newRHS.asTerm)))
+      case _ =>
+        report.error("@memoize is only allowed on def with a single parameter and no type parameters")
+        List(definition)
+  end transform
+
+  private def buildCache[K: Type, V: Type](name: String)(using Quotes): quotes.reflect.ValDef =
+    import quotes.reflect.*
+    val symbol = Symbol.newVal(Symbol.spliceOwner, name, TypeRepr.of[Map[K, V]], Flags.Private, Symbol.spliceOwner)
+    val rhs =
+      given Quotes = symbol.asQuotes
+      '{ Map.empty[K, V] }.asTerm
+    end rhs
+    ValDef(symbol, Some(rhs))
+  end buildCache
+
+  private def adaptDefToCache[K: Type, V: Type](using Quotes)(cache: Expr[Map[K, V]], key: Expr[K], rhs: Expr[V]): Expr[V] =
+    import quotes.reflect.*
+    '{ 
+        if $cache.contains($key) then 
+          $cache($key)
+        else
+          val res = $rhs
+          $cache($key) = res
+          res
+      }
+  end adaptDefToCache
+
+end memoize
---
+++ src/test/scala/steps/annotation/memoize.scala
@@ -0,0 +1,33 @@
+package steps.annotation
+
+import scala.annotation.experimental
+
+@experimental
+class FibonacciMemoize extends munit.FunSuite:
+
+  @memoize
+  def fib(n: Int): Int =
+    println(s"compute fib of $n")
+    if (n <= 1) return n
+    var prePrevious = 1
+    var previous = 1
+    var i = 2
+
+    while i < n do
+      val temp = prePrevious + previous
+      prePrevious = previous
+      previous = temp
+      i += 1
+
+    previous
+  end fib
+
+  test("fib compute one the result one time for the same entry"):
+      val output = new java.io.ByteArrayOutputStream
+      Console.withOut(output):
+          val r1 = fib(10)
+          val r2 = fib(10)
+          assert(r1 == r2) // check that the cache is indeed populated with the correct value
+      assertNoDiff(output.toString(), "compute fib of 10") // check that we indeed run the function once
+
+end FibonacciMemoize
