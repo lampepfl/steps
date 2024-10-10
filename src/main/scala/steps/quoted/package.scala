0041073ea1aa32eff06fe64e93ba3178bb88dc44
Hamza Remmal <hamza@remmal.dev>
2024-10-10 14:36:26 +0200

    Add extension method to check if a symbol is overriden in a template

---
+++ src/main/scala/steps/quoted/package.scala
@@ -0,0 +1,16 @@
+package steps.quoted
+
+import scala.quoted.*
+
+extension(using Quotes)(cls: quotes.reflect.Symbol)
+
+    /** Check if a given class, trait or object overrides a given definition
+      *
+      * @param cls The class, trait or object to inspect
+      * @param sym The symbol that should be overriden
+      * @return true if the definition is overriden, false otherwise
+      */
+    def overrides(sym: quotes.reflect.Symbol): Boolean =
+        sym.overridingSymbol(cls).exists
+
+end extension 
