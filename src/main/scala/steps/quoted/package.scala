023b5087a71e248761db708d5771b0da48a31c5e
Hamza Remmal <hamza@remmal.dev>
2024-10-10 14:45:47 +0200

    Make overrides inlined

--- src/main/scala/steps/quoted/package.scala
+++ src/main/scala/steps/quoted/package.scala
@@ -2,7 +2,7 @@ package steps.quoted
 
 import scala.quoted.*
 
-extension(using Quotes)(cls: quotes.reflect.Symbol)
+extension(using Quotes)(inline cls: quotes.reflect.Symbol)
 
     /** Check if a given class, trait or object overrides a given definition
       *
@@ -10,7 +10,7 @@ extension(using Quotes)(cls: quotes.reflect.Symbol)
       * @param sym The symbol that should be overriden
       * @return true if the definition is overriden, false otherwise
       */
-    def overrides(sym: quotes.reflect.Symbol): Boolean =
+    inline def overrides(inline sym: quotes.reflect.Symbol): Boolean =
         sym.overridingSymbol(cls).exists
 
 end extension 
