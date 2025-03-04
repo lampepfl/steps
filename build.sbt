61d79ba8adbac926a3f786174c86ac94351fa450
Natsu Kagami <nki@nkagami.me>
2025-03-04 14:17:12 +0100

    Update scala version to 3.6.3, add capture-checked stdlib

--- build.sbt
+++ build.sbt
@@ -1,23 +1,46 @@
-val scala3Version = "3.3.4"
+val scala3Version = "3.6.3"
 
-inThisBuild(List(
-  organization := "ch.epfl.lamp",
-  homepage := Some(url("https://lampepfl.github.io/steps")),
-  licenses := List("Apache-2.0" -> url(s"https://github.com/lampepfl/steps/blob/v${version.value}/LICENSE")),
-  developers := List(
-    Developer("natsukagami", "Natsu Kagami", "natsukagami@gmail.com", url("https://github.com/natsukagami")),
-    Developer("hamzaremmal", "Hamza Remmal", "hamza@remmal.net", url("https://remmal.net")),
-    Developer("bracevac", "Oliver Bračevac", "oliver@bracevac.org", url("https://bracevac.org"))
+inThisBuild(
+  List(
+    organization := "ch.epfl.lamp",
+    homepage := Some(url("https://lampepfl.github.io/steps")),
+    licenses := List(
+      "Apache-2.0" -> url(
+        s"https://github.com/lampepfl/steps/blob/v${version.value}/LICENSE"
+      )
+    ),
+    developers := List(
+      Developer(
+        "natsukagami",
+        "Natsu Kagami",
+        "natsukagami@gmail.com",
+        url("https://github.com/natsukagami")
+      ),
+      Developer(
+        "hamzaremmal",
+        "Hamza Remmal",
+        "hamza@remmal.net",
+        url("https://remmal.net")
+      ),
+      Developer(
+        "bracevac",
+        "Oliver Bračevac",
+        "oliver@bracevac.org",
+        url("https://bracevac.org")
+      )
+    )
   )
-))
-
+)
 
 lazy val root = project
   .in(file("."))
   .settings(
     name := "steps",
     scalaVersion := scala3Version,
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test,
+    libraryDependencies ++= Seq(
+      "org.scala-lang" %% "scala2-library-cc-tasty-experimental" % scala3Version,
+      "org.scalameta" %% "munit" % "0.7.29" % Test
+    ),
     Compile / doc / scalacOptions ++= Seq(
       "-groups"
     )
--- src/main/scala/steps/annotation/toString.scala
+++ src/main/scala/steps/annotation/toString.scala
@@ -3,50 +3,61 @@ package steps.annotation
 import scala.annotation.{experimental, MacroAnnotation}
 import scala.quoted.*
 
-
-/**
- * A macro annotation that automatically generates a custom `toString` method for a class.
- * 
- * The `@toString` annotation can be applied to a class, object, or trait. When applied, it overrides the `toString`
- * method to include the class name and the values of all fields marked as `ParamAccessor`.
- * 
- * If the class already defines or overrides the `toString` method, the annotation will emit a warning indicating that
- * the annotation is not necessary. The existing `toString` method will remain unchanged.
- *
- * Example usage:
- * {{{
- * @toString
- * class MyClass(val a: Int, val b: String)
- *
- * val instance = MyClass(1, "hello")
- * println(instance.toString) // Output: MyClass(1, hello)
- * }}}
- *
- * The generated `toString` method produces output in the format: `ClassName(field1, field2, ...)`.
- *
- * @note This annotation requires Scala's `experimental` flag to be enabled, or it can be used within a scope
- *       marked as experimental (i.e., using `import scala.annotation.experimental`). This is necessary because it 
- *       relies on experimental macro annotation features.
- */
+/** A macro annotation that automatically generates a custom `toString` method
+  * for a class.
+  *
+  * The `@toString` annotation can be applied to a class, object, or trait. When
+  * applied, it overrides the `toString` method to include the class name and
+  * the values of all fields marked as `ParamAccessor`.
+  *
+  * If the class already defines or overrides the `toString` method, the
+  * annotation will emit a warning indicating that the annotation is not
+  * necessary. The existing `toString` method will remain unchanged.
+  *
+  * Example usage:
+  * {{{
+  * @toString
+  * class MyClass(val a: Int, val b: String)
+  *
+  * val instance = MyClass(1, "hello")
+  * println(instance.toString) // Output: MyClass(1, hello)
+  * }}}
+  *
+  * The generated `toString` method produces output in the format:
+  * `ClassName(field1, field2, ...)`.
+  *
+  * @note
+  *   This annotation requires Scala's `experimental` flag to be enabled, or it
+  *   can be used within a scope marked as experimental (i.e., using `import
+  *   scala.annotation.experimental`). This is necessary because it relies on
+  *   experimental macro annotation features.
+  */
 @experimental
 final class toString extends MacroAnnotation:
 
-  /**
-   * Transforms the annotated class to add a custom `toString` method.
-   * 
-   * If the class already overrides `toString`, a warning is emitted and no changes are made.
-   * Otherwise, this annotation adds a new `toString` method that returns a string representation of
-   * the class name and its fields.
-   * 
-   * @param tree The abstract syntax tree (AST) of the annotated class, object, or trait.
-   * @return The transformed class definition, with the generated `toString` method if applicable.
-   */
-  override def transform(using Quotes)(tree: quotes.reflect.Definition): List[quotes.reflect.Definition] = 
+  /** Transforms the annotated class to add a custom `toString` method.
+    *
+    * If the class already overrides `toString`, a warning is emitted and no
+    * changes are made. Otherwise, this annotation adds a new `toString` method
+    * that returns a string representation of the class name and its fields.
+    *
+    * @param tree
+    *   The abstract syntax tree (AST) of the annotated class, object, or trait.
+    * @return
+    *   The transformed class definition, with the generated `toString` method
+    *   if applicable.
+    */
+  override def transform(using quotes: Quotes)(
+      tree: quotes.reflect.Definition,
+      _companion: Option[quotes.reflect.Definition]
+  ): List[quotes.reflect.Definition] =
     import quotes.reflect.*
     val toStringSym = Symbol.requiredMethod("java.lang.Object.toString")
     tree match
       case _: ClassDef if toStringSym.overridingSymbol(tree.symbol).exists =>
-        report.warning(s"@toString is not necessary since toString is defined in ${tree.symbol}")
+        report.warning(
+          s"@toString is not necessary since toString is defined in ${tree.symbol}"
+        )
         List(tree)
       case cls: ClassDef if cls.symbol.flags.is(Flags.Trait) =>
         report.error(s"@toString is not supported on traits")
@@ -62,28 +73,41 @@ final class toString extends MacroAnnotation:
             Select(This(cls), vdef.symbol).asExpr
         }
 
-        val toStringOverrideSym = Symbol.newMethod(cls, toStringSym.name, toStringSym.info, Flags.Override, Symbol.noSymbol)
+        val toStringOverrideSym = Symbol.newMethod(
+          cls,
+          toStringSym.name,
+          toStringSym.info,
+          Flags.Override,
+          Symbol.noSymbol
+        )
 
         def toStringOverrideDefBody(argss: List[List[Tree]]): Option[Term] =
           given Quotes = toStringOverrideSym.asQuotes
           Some(toStringExpr(className, fields).asTerm)
 
         val toStringDef = DefDef(toStringOverrideSym, toStringOverrideDefBody)
-        List(ClassDef.copy(tree)(className, ctr, parents, self, toStringDef :: body))
+        List(
+          ClassDef
+            .copy(tree)(className, ctr, parents, self, toStringDef :: body)
+        )
       case _ =>
         report.errorAndAbort("@toString is only supported on class")
   end transform
 
-  /**
-   * Helper method to create the string representation of the class.
-   *
-   * Constructs a string in the format: `ClassName(field1, field2, ...)`.
-   *
-   * @param className The name of the class.
-   * @param thisFields The list of expressions representing the class fields.
-   * @return A quoted expression representing the final string.
-   */
-  private def toStringExpr(className: String, thisFields: List[Expr[Any]])(using Quotes): Expr[String] =
+  /** Helper method to create the string representation of the class.
+    *
+    * Constructs a string in the format: `ClassName(field1, field2, ...)`.
+    *
+    * @param className
+    *   The name of the class.
+    * @param thisFields
+    *   The list of expressions representing the class fields.
+    * @return
+    *   A quoted expression representing the final string.
+    */
+  private def toStringExpr(className: String, thisFields: List[Expr[Any]])(using
+      Quotes
+  ): Expr[String] =
     val fieldsSeq = Expr.ofSeq(thisFields)
     val prefix = Expr(className + "(")
     '{ $fieldsSeq.mkString($prefix, ", ", ")") }
