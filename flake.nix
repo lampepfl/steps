c7d323a13d88d660ccd9ba5c22b66aed5d35b2ea
Natsu Kagami <nki@nkagami.me>
2026-02-20 15:04:22 +0100

    Force all scala tools in the Nix Flake to use the same JRE

--- flake.nix
+++ flake.nix
@@ -6,7 +6,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         # To import a flake module
@@ -15,18 +16,45 @@
         # 3. Add here: foo.flakeModule
 
       ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          jre = pkgs.jdk21;
+          withJRE = jre: pkg: (pkg.override { inherit jre; });
+          scala-tools = builtins.map (withJRE jre) (
+            with pkgs;
+            [
+              scala-cli
+              metals
+              scalafmt
+              sbt
+            ]
+          );
+        in
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        # packages.default = pkgs.hello;
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ jdk21 scala-cli metals scalafmt sbt ];
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          # packages.default = pkgs.hello;
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ jre ] ++ scala-tools;
+          };
         };
-      };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
